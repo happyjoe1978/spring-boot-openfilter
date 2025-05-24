e28ac2cd806920c1af11f4578dd496c167e5e6e9
13220202000 <zh616211@163.com>
2025-05-24 19:53:45 +0800

    init

---
+++ src/test/java/com/orangstar/spring_boot_openapi_filter/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orangstar.spring_boot_openapi_filter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
